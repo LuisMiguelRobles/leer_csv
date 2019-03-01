1b1694811cf9276498ea53c8ff2d4753b96e0d16
LuisMiguelRobles <luismirobles97@gmail.com>
2019-03-01 18:35:42 -0500

    Corrigiendo Errores

--- application.py
+++ application.py
@@ -3,46 +3,42 @@ from flask import request
 from flask import render_template
 from werkzeug.utils import secure_filename
 import csv
+from flask_bootstrap import Bootstrap
+import numbers
+import re
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+regex = re.compile(r'[0-9]+')
+"""regex = re.compile(r'[^.0-9][0-9]+')"""
 dataNum = []
+datos=[]
 pares = []
 impares = []
-
 
 
 def ca(fileName):
     with open('./'+fileName) as f:
 
-        reader = csv.reader(f)
+        reader = csv.reader(f, delimiter=';')
         """dataNum = list(reader)"""
         global dataNum
-        dataNum = [int(item) for sublist in list(reader) for item in sublist]
-        
+        dataNum = [int(item) for sublist in list(reader) for item in sublist if regex.search(item)]
         dataNum.sort()
+        print(dataNum)
         for x in dataNum:
+            print(type(x))
             if x % 2 == 0:
                 pares.append(x)
             else:
                 impares.append(x)
 
 
-
-"""def flattener(lst):
-    return [int(item) for sublist in lst for item in sublist]"""
-
-
-
-
-
 @app.route('/')
 def index():
-    """return render_template('Index.html')"""
-
-    result = dataNum
-    return render_template('Index.html', lista=result, pares=pares,impares=impares)
+    return render_template('Index.html')
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
@@ -57,7 +53,9 @@ def upload_file():
             """return 'file uploaded successfully'"""
             return render_template('Data.html', data=dataNum, pares=pares, impares=impares)
         else:
-            return 'El archivo debe ser .csv'
+            return render_template('Error.html')
+
+
 
 
 
